fa634badb1504c85937a81ee02a69e663d51b120
Doğukan Keskin <dogukankeskin@Mac.hgw.local>
2026-02-24 17:57:29 +0300

    fix: final adjustment to compression import with esModuleInterop

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import * as compression from 'compression';
+import compression from 'compression';
 
 async function bootstrap() {
     const logger = new Logger('Bootstrap');
